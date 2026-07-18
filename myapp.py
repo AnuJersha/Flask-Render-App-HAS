0a7810c72b5411a08f573fb2358d6b51881f7dbb
AnuJersha <jershaanu48@gmail.com>
2026-07-18 10:18:21 +0530

    Add files via upload

--- myapp.py
+++ myapp.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Render PaaS Government"
+    return "Hello from Render PaaS Government Polytechnic"
 
 @app.route("/about")
 def about():
